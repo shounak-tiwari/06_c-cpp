9a21c41e9854c182529b26c599680cce58ed9fda
Akash Tiwari <154492266+shounak-tiwari@users.noreply.github.com>
2025-03-12 19:20:25 +0530

    Add files via upload

---
+++ Mar-12/01_input.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+// standard input output 
+
+void main(){
+	// declare three int type variable 
+	
+	int num1,num2,num3;
+	
+	// print message on console 
+	printf("Enter the value of number 1 , number 2 , number 3 : ");
+	
+	// scanf for input 
+	
+	scanf("%d%d%d",&num1,&num2,&num3);
+	
+	// print all values 
+	
+	printf("\nThe value of number 1 : %d",num1);
+	
+	printf("\nThe value of number 2 : %d",num2);
+	
+	printf("\nThe value of number 3 : %d",num3);
+	
+}
